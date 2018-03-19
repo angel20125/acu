7809138cd03b67a4aa14717a81b663db1ab2e5df
José Avilés <joseaviles230796@gmail.com>
2018-03-19 14:36:07 -0430

    Calendar In Row

--- resources/views/main/admin_dashboard.blade.php
+++ resources/views/main/admin_dashboard.blade.php
@@ -42,51 +42,49 @@
 	</div>
 	
 </div>
+
+<br>
+
 <!-- Calendar -->
 <div class="row"> 
-	
+	@php $startDate = DateTime::createFromFormat("Y-m-d",gmdate("Y-m-d"))->modify('first day of this month')->format("w"); @endphp
+	<div class="table-responsive">
+		<table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
+		    <thead>
+		        <tr>
+		        	<th>Domingo</th>
+		        	<th>Lunes</th>
+		        	<th>Martes</th>
+		        	<th>Miércoles</th>
+		        	<th>Jueves</th>
+		        	<th>Viernes</th>
+		        	<th>Sábado</th>
+		        </tr>
+		    </thead>
+		    </tbody>
+		    	@php $count=0; $countDay=1; @endphp
+		    	@for($i=1; $i <= 5; $i++)
+		        <tr>
+		        	@for($j=1; $j <= 7; $j++)
+		        		@if($count < $startDate)
+		        			<td>Día pasado</td>
+		        		@elseif($count >= $startDate && $countDay <= DateTime::createFromFormat("Y-m-d",gmdate("Y-m-d"))->modify('last day of this month')->format("d"))
+		        			<td>{{$countDay}}/{{DateTime::createFromFormat("m/Y",gmdate("m/Y"))->format("m/Y")}}</td>
+		        			@php $countDay++; @endphp
+		        		@elseif($countDay > DateTime::createFromFormat("Y-m-d",gmdate("Y-m-d"))->modify('last day of this month')->format("d"))
+		        			<td>Día futuro</td>
+		        		@endif
+		        		@php $count++; @endphp
+		        	@endfor
+		        </tr>
+		        @endfor
+		    </tbody>
+		</table>
+	</div>
 </div>
 
 @endsection
 @section('script')
-
-<br>
-
-@php $startDate = DateTime::createFromFormat("Y-m-d",gmdate("Y-m-d"))->modify('first day of this month')->format("w"); @endphp
-
-<div class="table-responsive">
-	<table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%">
-	    <thead>
-	        <tr>
-	        	<th>Domingo</th>
-	        	<th>Lunes</th>
-	        	<th>Martes</th>
-	        	<th>Miércoles</th>
-	        	<th>Jueves</th>
-	        	<th>Viernes</th>
-	        	<th>Sábado</th>
-	        </tr>
-	    </thead>
-	    </tbody>
-	    	@php $count=0; $countDay=1; @endphp
-	    	@for($i=1; $i <= 5; $i++)
-	        <tr>
-	        	@for($j=1; $j <= 7; $j++)
-	        		@if($count < $startDate)
-	        			<td>Día pasado</td>
-	        		@elseif($count >= $startDate && $countDay <= DateTime::createFromFormat("Y-m-d",gmdate("Y-m-d"))->modify('last day of this month')->format("d"))
-	        			<td>{{$countDay}}/{{DateTime::createFromFormat("m/Y",gmdate("m/Y"))->format("m/Y")}}</td>
-	        			@php $countDay++; @endphp
-	        		@elseif($countDay > DateTime::createFromFormat("Y-m-d",gmdate("Y-m-d"))->modify('last day of this month')->format("d"))
-	        			<td>Día futuro</td>
-	        		@endif
-	        		@php $count++; @endphp
-	        	@endfor
-	        </tr>
-	        @endfor
-	    </tbody>
-	</table>
-</div>
 
 @endsection
 
